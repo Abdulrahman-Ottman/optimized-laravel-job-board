99ae7ff9319c53c9b8d0069b43308b5c8233025c
Abdulrahman Othman <abdulrahman.othamn.sy@gmail.com>
2024-10-07 15:12:13 +0300

    posting a job logic

--- resources/views/components/wide-job-card.blade.php
+++ resources/views/components/wide-job-card.blade.php
@@ -1,12 +1,14 @@
 @props(['job'])
 <x-panel class="flex gap-x-6">
     <div>
-        <x-employer-logo />
+        <x-employer-logo :employer="$job->employer"/>
     </div>
     <div class="flex-1 flex flex-col">
         <a href="" class="self-start text-sm text-gray-400 ">{{$job->employer->name}}</a>
 
-            <h3 class="font-bold text-xl mt-2 group-hover:text-blue-600 transition-colors duration-400">{{$job->title}}</h3>
+            <h3 class="font-bold text-xl mt-2 group-hover:text-blue-600 transition-colors duration-400">
+                <a href="{{$job->url}}" target="_blank">{{$job->title}}</a>
+            </h3>
             <p class="text-sm text-gray-400 mt-auto">{{$job->salary}}</p>
     </div>
     <div>
